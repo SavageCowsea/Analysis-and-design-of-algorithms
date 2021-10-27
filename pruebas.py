76e96de344747fceddd578e182e8890027c17363
Eduardo Santos <esantosr@unal.edu.co>
2021-10-26 20:55:55 -0500

    Module 3

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,23 @@
-for i in range(1, 4):
-    for j in range(1, 5):
-        if (i+2*j+3) == 12 and i < j:
-            print(i, j)
+def f(l: list, s: int, n: int) -> int:
+    if s == n:
+        yield l
+    else:
+        for i in range(1, n):
+
+            l[s] = i
+            yield from f(l, s+1, n)
+
+
+""" print(next(f([0]*4, 0, len([0]*4))))
+print(next(f([0]*4, 0, len([0]*4))))
+print(next(f([0]*4, 0, len([0]*4))))
+print(next(f([0]*4, 0, len([0]*4)))) """
+
+jmm = (f([0]*4, 0, 4))
+
+while(True):
+
+    try:
+        print(next(jmm))
+    except:
+        break
--- robotCartografo.py
+++ robotCartografo.py
@@ -1,1 +1,48 @@
-git remote add origin https://git@github.com/SavageCowsea/protractor-workshop-2021.git
+def check(possible, ad: list) -> bool:
+    melo = True
+    for i, ii in enumerate(possible):
+        for j, ji in enumerate(ad[i]):
+            if(i != j and ii == possible[j] and ji == 1):
+                melo = False
+                return melo
+    return melo
+
+
+def f(l: list, s: int, n: int) -> int:
+    if s == n:
+        yield l
+    else:
+        for i in range(n):
+            l[s] = i
+            yield from f(l, s+1, n)
+
+
+def run():
+    # sentinel = ''
+    # print(list('\n'.join(iter(input, sentinel)).replace('\n',)))
+    a_matrix = []
+    colors = int(input())
+    for rows in range(colors):
+        a_matrix.append(list(map(int, input().split())))
+
+    # print(a_matrix)
+    o = 1
+    a = f([0]*colors, 0, o)
+    while(True):
+
+        try:
+            b = next(a)
+            print(b)
+            if (check(b, a_matrix) == True):
+
+                print(len(set(b)))
+                break
+        except StopIteration:
+            o += 1
+            a = f([0]*colors, 0, o)
+
+
+if __name__ == '__main__':
+    x = int(input())
+    for _ in range(x):
+        run()
